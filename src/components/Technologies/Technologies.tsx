ed3f7bb17add042f7526ecc78d658a21091d1514
Rakibul Haque <rh.turzo@gmail.com>
2026-09-14 00:19:46 +0600

    last

--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -5,7 +5,10 @@ import { toast } from "react-toastify";
 import StackSidebar from "./StackSidebar";
 
 const techFetch = async (): Promise<ITechItem[]> => {
-  const res = await fetch("/data.json");
+  const res = await fetch(`${import.meta.env.BASE_URL}data.json`);
+  if (!res.ok) {
+    throw new Error("Unable to load technologies.");
+  }
   const data = await res.json();
   return data;
 };
